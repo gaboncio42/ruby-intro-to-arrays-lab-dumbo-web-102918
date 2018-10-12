0d38e4003a917aa856b24310562af85fea359a03
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-10-12 17:00:51 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -3,3 +3,8 @@ def instantiate_new_array
   return instantiate_new_array
 end
 
+def array_with_two_elements
+  array_with_two_elements = ["Lebron James", "Kevin Durant"]
+  return array_with_two_elements
+end
+
